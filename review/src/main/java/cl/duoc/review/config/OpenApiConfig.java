73b92ef9f40161e073b75b037d26748e7b5bb9eb
WilianZela <wi.zela@duocuc.cl>
2026-06-30 12:14:23 -0400

    Carpetas  temporales en config y controler

---
+++ review/src/main/java/cl/duoc/review/config/OpenApiConfig.java
@@ -0,0 +1,9 @@
+package main.java.cl.duoc.review.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+public class OpenApiConfig {
+
+}
---
+++ review/src/main/java/cl/duoc/review/controller/ReviewController.java
@@ -0,0 +1,22 @@
+package main.java.cl.duoc.review.controller;
+
+import cl.duoc.login.dto.ApiResponse;
+import cl.duoc.login.dto.UserCredentialsDTO;
+import cl.duoc.login.dto.UserDTO;
+import cl.duoc.login.model.User;
+import cl.duoc.login.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class ReviewController {
+
+}
